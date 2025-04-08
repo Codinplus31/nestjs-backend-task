91c9f611efd1e6ba23c009f41ab98e0492a80a52
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-04-08 10:23:55 +0100

    Update auth-response.dto.ts

--- src/auth/dto/auth-response.dto.ts
+++ src/auth/dto/auth-response.dto.ts
@@ -4,9 +4,9 @@ import { User } from "../../users/entities/user.entity"
 @ObjectType()
 export class AuthResponse {
   @Field()
-  accessToken: string
+  accessToken!: string
 
   @Field(() => User)
-  user: User
+  user!: User
 }
 

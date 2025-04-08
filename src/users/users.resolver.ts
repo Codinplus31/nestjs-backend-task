6f07b3d08f2bbbe61b56f19d275d781f87df0d85
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-04-08 11:37:31 +0100

    Update users.resolver.ts

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,6 @@
 import { Resolver, Query, Args, ID } from "@nestjs/graphql"
 import { UseGuards } from "@nestjs/common"
-import type { UsersService } from "./users.service"
+import  { UsersService } from "./users.service"
 import { User } from "./entities/user.entity"
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard"
 import { CurrentUser } from "../common/decorators/current-user.decorator"

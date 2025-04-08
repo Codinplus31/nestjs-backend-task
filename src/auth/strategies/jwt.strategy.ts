61f413952a3da74ba7e4e54af83371519c982201
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-04-08 11:39:35 +0100

    Update jwt.strategy.ts

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@nestjs/common"
 import { PassportStrategy } from "@nestjs/passport"
 import { ExtractJwt, Strategy } from "passport-jwt"
-import type { ConfigService } from "@nestjs/config"
-import type { UsersService } from "../../users/users.service"
+import  { ConfigService } from "@nestjs/config"
+import  { UsersService } from "../../users/users.service"
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {

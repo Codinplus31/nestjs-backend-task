a127ee8e7ed027b27f5e7bebe6de7ee670ada2d7
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-04-08 13:10:24 +0100

    Update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -8,7 +8,7 @@ export class User {
   @Field()
   email!: string;
 
-  @Field({ nullable: true })
+  @Field(() => String, { nullable: true })
   biometricKey!: string | null; // Allow null values
 
   @Field()
